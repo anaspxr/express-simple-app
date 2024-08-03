cd670cf9cb714e12c675123cc6d123d68d8e9285
ANAS <anaspappadan@gmail.com>
2024-08-03 12:23:39 +0530

    api: install and enable cors

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
+const cors = require("cors");
 const userRouter = require("./routes");
+require("dotenv").config();
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 
 const PORT = process.env.PORT;
