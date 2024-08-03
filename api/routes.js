7855eb06444b725211ffaa4724897de1f3eaec92
ANAS <anaspappadan@gmail.com>
2024-08-03 14:37:40 +0530

    api: create controllers for get data, create and delete user

--- api/routes.js
+++ api/routes.js
@@ -1,38 +1,15 @@
 const router = require("express").Router();
 const fs = require("node:fs");
+const userControllers = require("./controllers");
 
-router.get("/users", (_, res) => {
-  fs.readFile("./data/users.json", "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.header({ "Content-Type": "application/json" });
-      res.send(data);
-    }
-  });
-});
+router
+  .route("/users")
+  .get(userControllers.handleGet)
+  .post(userControllers.handleCreate);
 
-router.post("/users", (req, res) => {
-  fs.readFile("./data/users.json", "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-      res.sendStatus(400);
-    } else {
-      const parsed = JSON.parse(data);
-      const newUser = req.body;
-      newUser.id = Date.now();
-      parsed.push(newUser);
-      const newData = JSON.stringify(parsed);
-      fs.writeFile("./data/users.json", newData, "utf-8", (err) => {
-        if (err) {
-          console.log(err);
-          res.sendStatus(400);
-        } else {
-          res.sendStatus(201);
-        }
-      });
-    }
-  });
-});
+router
+  .route("/users/:id")
+  .get(userControllers.handleUserView)
+  .delete(userControllers.handleDelete);
 
 module.exports = router;
